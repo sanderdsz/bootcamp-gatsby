e4e5f9e45e3bdb53f9da55dd93a2bd4a8c9e749c
sanderdsz <sanderdsz@gmail.com>
2020-05-06 20:11:59 -0300

    link router for each blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,37 +1,42 @@
 import React from "react"
-import {graphql, useStaticQuery} from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 import Layout from "../components/layout"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
-  query {
-    allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            title
-            date
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
           }
         }
       }
     }
-  }
-`)
+  `)
 
-return (
+  return (
     <div>
       <Layout>
         <h1>Blog</h1>
         <ol>
-        {data.allMarkdownRemark.edges.map((edge) => {
-          return ( 
+          {data.allMarkdownRemark.edges.map(edge => {
+            return (
               <li>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <p>{edge.node.frontmatter.date}</p>
-            </li>
-          )
-        })}
+                <Link to={`/blog/${edge.node.fields.slug}`}>
+                  <h2>{edge.node.frontmatter.title}</h2>
+                  <p>{edge.node.frontmatter.date}</p>
+                </Link>
+              </li>
+            )
+          })}
         </ol>
       </Layout>
     </div>
