908ad8f887f70ff9803bec74fd1a1e953a24d284
sander <sander@sanders-MacBook-Pro.local>
2020-05-13 21:33:36 -0300

    blog list and footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,24 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
+import footerStyles from "./footer.module.scss"
+
 const Footer = () => {
   const data = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        author
+    query {
+      site {
+        siteMetadata {
+          author
+        }
       }
     }
-  }
-`)
+  `)
 
   return (
-    <div>
+    <footer className={footerStyles.footer}>
       <p> Criado por {data.site.siteMetadata.author}, 2020.</p>
-    </div>
+    </footer>
   )
 }
 
 export default Footer
- 
