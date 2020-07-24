77bb9b0602fe7322a2cca42a1c309827917dae2b
sanderdsz <sanderdsz@gmail.com>
2020-07-24 00:22:32 -0300

    buttons outline scss

--- src/pages/shiver/docs/index.js
+++ src/pages/shiver/docs/index.js
@@ -3,34 +3,38 @@ import React from "react"
 import "../../../components/organisms/sideNavigation"
 import SideNavigation from "../../../components/organisms/sideNavigation"
 
-import Button from '../../../components/atoms/button'
+import Button from "../../../components/atoms/button"
 
 const Docs = () => {
-  return <div style={{margin: 8}}>
-   <SideNavigation>
-     Hello world
-   </SideNavigation>
-   {/*
-   <Button label="Basic" variant="basic" />
-    <Button label="Link" variant="link" />
-    <Button label="Secondary" variant="secondary" />
-    <Button label="Danger" variant="danger" />
-    <Button label="Disabled" disabled={true} />
-    <Button label="Primary" variant="primary" />
-    <Button label="Outline" variant="outline" />
-    <Button label="Medium" variant="primary" size="medium" />
-    <Button label="Large" variant="outline" size="large" />
-   */}
-    <div style={{margin: 8}}>
-      <Button label="Default" variant="default"/>
+  return (
+    <div style={{ margin: 8 }}>
+      <SideNavigation>Hello world</SideNavigation>
+      <div style={{ margin: 8 }}>
+        <Button label="Default" variant="default" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Primary" variant="primary" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Success" variant="success" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Warning" variant="warning" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Danger" variant="danger" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Disabled" variant="disabled" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Default Outline" variant="default—outline" />
+      </div>
+      <div style={{ margin: 8 }}>
+        <Button label="Primary Outline" variant="primary—outline" />
+      </div>
     </div>
-    <div style={{margin: 8}}>
-      <Button label="Primary" variant="primary"/>
-    </div>
-    <div style={{margin: 8}}>
-      <Button label="Warning" variant="warning"/>
-    </div>
-  </div>
+  )
 }
 
 export default Docs
